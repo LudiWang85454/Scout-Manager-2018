4a277a22aba5f47bcbca39e75b80e9b47747d95c
Carl <ccsaposs+github@gmail.com>
2018-03-27 16:43:52 -0700

    Add slackNotifications script and call in databaseListener

--- databaseListener.py
+++ databaseListener.py
@@ -41,6 +41,7 @@ def stream_match_handler(message):
 			cycle = 0
 		if message["data"] != int(cycle) and if home == "/home/citrus": # Prevents double slack notification
 			subprocess.call("python3 " +os.path.join(home, "Desktop/scoutNotSent.py"), shell=True)
+			subprocess.call(os.path.join(home, "Desktop/sendSlackNotifications.py"), shell=True)
 			with open(os.path.join(home, 'Downloads/data/lastSentMatch.txt'), 'w') as f:
 				f.write(str(message["data"]))
 
---
+++ sendSlackNotifications.py
@@ -0,0 +1,42 @@
+from slackclient import SlackClient
+import pyrebase
+
+with open('/home/carl/Downloads/data/apikey.txt', 'r') as f:
+	apikey = f.read()
+slack = SlackClient(apikey)
+
+url = 'scouting-2018-9023a'
+
+config = {
+	'apiKey': 'mykey',
+	'authDomain': url + '.firebaseapp.com',
+	'databaseURL': 'https://' + url + '.firebaseio.com/',
+	'storageBucket': url + '.appspot.com'
+}
+
+firebase = pyrebase.initialize_app(config)
+db = firebase.database()
+
+currentMatch = db.child('currentMatchNum').get().val()
+slackProfiles = db.child('activeSlackProfiles').get().val()
+
+notifications = {}
+for user in slackProfiles:
+	notifications[user] = [x for x in slackProfiles[user]['starredMatches'] if x >= currentMatch and (x-currentMatch <= slackProfiles[user]['notifyInAdvance'])]
+
+for user in notifications:
+	text = ""
+	for match in notifications[user]:
+		if match-currentMatch == 0:
+			text += 'Match '+str(match)+' is next.\n'
+		elif match-currentMatch == 1:
+			text += 'Match '+str(match)+' is 1 match away.\n'
+		else:
+			text += 'Match '+str(match)+' is '+str(match-currentMatch)+' match(es) away.\n'
+	if text != "":
+		text = text[:-1]
+		print(slack.api_call('chat.postMessage',
+			channel = user,
+			as_user = True,
+			text = text,
+			))
