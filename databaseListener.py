3bcd2d2e2ea67250741c1d94fa8cd593a7624482
ccsaposs <ccsaposs+github@gmail.com>
2018-04-01 14:12:44 -0700

    Move slack notifications to separate script

--- databaseListener.py
+++ databaseListener.py
@@ -37,7 +37,6 @@ def stream_match_handler(message):
 		if message["data"] != int(cycle) and home == "/home/citrus": # Prevents double slack notification
 			if message["data"] != 0:
 				subprocess.call("python3 " +os.path.join(home, "scoutManager/scoutNotSent.py"), shell=True)
-			#subprocess.call(os.path.join(home, "scoutManager/sendSlackNotifications.py"), shell=True)
 			with open(os.path.join(home, 'Downloads/data/lastSentMatch.txt'), 'w') as f:
 				f.write(str(message["data"]))
 
---
+++ databaseListenerSlack.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python -p
+import os
+import pyrebase
+import subprocess
+
+home = os.path.expanduser('~')
+
+url = 'scouting-2018-9023a'
+
+config = {
+	'apiKey': 'mykey',
+	'authDomain': url + '.firebaseapp.com',
+	'databaseURL': 'https://' + url + '.firebaseio.com/',
+	'storageBucket': url + '.appspot.com'
+}
+
+firebase = pyrebase.initialize_app(config)
+db = firebase.database()
+
+def stream_match_handler(message):
+	if type(message["data"]) == int:
+		with open(os.path.join(home, 'Downloads/data/lastSentMatch.txt'), 'r') as f:
+			cycle = f.read()
+		if cycle == "":
+			cycle = 0
+		if message["data"] != int(cycle) and home == "/home/citrus": # Prevents double slack notification
+			isubprocess.call(os.path.join(home, "scoutManager/sendSlackNotifications.py"), shell=True)
+			with open(os.path.join(home, 'Downloads/data/lastSentMatch.txt'), 'w') as f:
+				f.write(str(message["data"]))
+
+stream3 = db.child("currentMatchNum").stream(stream_match_handler)
