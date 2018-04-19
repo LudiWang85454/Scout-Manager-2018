00f20b1b47d7b0a584d50e9b87f108d57f45cef0
ccsaposs <32380359+ccsaposs@users.noreply.github.com>
2018-04-19 05:44:08 -0500

    Change updateQRCode to use server database for SPR (#8)
    * Use the SPR firebase
    
    * Cleanup updateQRCode.py

--- updateQRCode.py
+++ updateQRCode.py
@@ -17,6 +17,18 @@ config = {
 firebase = pyrebase.initialize_app(config)
 db = firebase.database()
 
+QRurl = 'server-2018-3209b'
+
+QRconfig = {
+	'apiKey': 'mykey',
+	'authDomain': QRurl + '.firebaseapp.com',
+	'databaseURL': 'https://' + QRurl + '.firebaseio.com/',
+	'storageBucket': QRurl + '.appspot.com'
+}
+
+QRfirebase = pyrebase.initialize_app(QRconfig)
+QRdb = QRfirebase.database()
+
 with open(os.path.join(home, 'Downloads/data/QRAssignments.json'), 'r') as f:
 	data = json.load(f)
 
@@ -25,7 +37,7 @@ letters = data['letters']
 print("Pulling data from firebase...")
 availableScouts = [scout for scout, val in db.child('availability').get().val().items() if val == 1]
 cycleNum = db.child('cycleCounter').get().val()
-sprs = db.child("SPRs").get().val()
+sprs = QRdb.child("SPRs").get().val()
 print("Done pulling data.")
 sprsAll = {scout:sprs[scout] if scout in sprs else 0 for scout in availableScouts}
 availableSPRs = {scout:spr for scout, spr in sprsAll.items() if scout in availableScouts}
