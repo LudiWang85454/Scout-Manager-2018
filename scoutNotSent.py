012b43ad22f80062af977e9fe712b974e23fcabe
ccsaposs <ccsaposs+github@gmail.com>
2018-04-19 10:42:43 -0700

    Update SNS with new lunchtime features

--- scoutNotSent.py
+++ scoutNotSent.py
@@ -20,17 +20,23 @@ config = {
 firebase = pyrebase.initialize_app(config)
 db = firebase.database()
 
+
 print("Getting data from firebase...")
 #Finds the number of matches already played using the currentMatchNum key
 currentMatch = db.child('currentMatchNum').get().val()
+currentCycle = db.child('cycleCounter').get().val()
 
 #Finds all of the tempTIMDs in the sent folder
 timds = list(db.child("TempTeamInMatchDatas").shallow().get().val())
 dTimds = [x for x in timds if int(x.split("Q")[1].split("-")[0]) == currentMatch-2]
 diagnostics = {}
-diagnostics['mode'] = {dTimds[ix]:db.child("TempTeamInMatchDatas/"+str(dTimds[ix])+"/mode").get().val() for ix in range(len(dTimds))}
-diagnostics['cycle'] = {dTimds[ix]:db.child("TempTeamInMatchDatas/"+str(dTimds[ix])+"/cycle").get().val() for ix in range(len(dTimds))}
-print("Done getting data from firebase.")
+diagnostics['mode'] = {int(dTimds[ix].split("-")[1]):db.child("TempTeamInMatchDatas/"+str(dTimds[ix])+"/mode").get().val() for ix in range(len(dTimds))}
+diagnostics['cycle'] = {int(dTimds[ix].split("-")[1]):db.child("TempTeamInMatchDatas/"+str(dTimds[ix])+"/cycle").get().val() for ix in range(len(dTimds))}
+diagnostics['names'] = {int(dTimds[ix].split("-")[1]):db.child("TempTeamInMatchDatas/"+str(dTimds[ix])+"/scoutName").get().val() for ix in range(len(dTimds))}
+print("Done getting data from firebase.") 
+
+def getNameFromID(i):
+	return str(diagnostics['names'][i]) if diagnostics['cycle'][i] == currentCycle else str(diagnostics['names'][i])+"*"
 
 #Finds all of the scouts who are supposed to be sending to cross reference with
 expected = [x for x in range(1,19)]
@@ -61,7 +67,7 @@ slackScoutNotSent = ''
 for num in expected:
 	sent = scoutNotSent[num]
 	if sent:
-		slackScoutNotSent += ('Scout ' + str(num) + ': ')
+		slackScoutNotSent += ('Scout ' + str(num) + " (" + getNameFromID(num)+'): ')
 		for match in sent:
 			slackScoutNotSent += str(match)
 			if match != sent[-1]:
@@ -82,7 +88,7 @@ elif list(diagnostics['mode'].values()).count(modeMode) != len(diagnostics['mode
 	diagnosticsMessage += "> Current detected mode: " + str(modeMode) + "\n"
 	for k, v in diagnostics['mode'].items():
 		if v != modeMode:
-			diagnosticsMessage += "Scout " + (str(k).split("-")[1]) + ": " + str(v) + "\n"
+			diagnosticsMessage += ('Scout ' + str(k) + " (" + getNameFromID(k)+'): ') + str(v) + "\n"
 
 if modeCycle == None:
 	diagnosticsMessage += "Please check cycle on all tablets.\n"
@@ -90,7 +96,10 @@ elif list(diagnostics['cycle'].values()).count(modeCycle) != len(diagnostics['cy
 	diagnosticsMessage += "> Current detected cycle: " + str(modeCycle) + "\n"
 	for k, v in diagnostics['cycle'].items():
 		if v != modeCycle:
-			diagnosticsMessage += "Scout " + (str(k).split("-")[1]) + ": " + str(v) + "\n"
+			diagnosticsMessage += ('Scout ' + str(k) + " (" + getNameFromID(k)+'): ') + str(v) + "\n"
+
+
+#diagnosticsMessage = ''
 
 status = '#f1ad1d'
 if slackScoutNotSent == '' and diagnosticsMessage == '':
@@ -117,7 +126,7 @@ userIDs = [
 
 #Sends a message to every user
 for user in userIDs:
-	slack.api_call('chat.postMessage',
+	print(slack.api_call('chat.postMessage',
 		channel = user,
 		as_user = True,
 		icon_url = 'https://i.imgur.com/1snON7W.png',
@@ -125,5 +134,5 @@ for user in userIDs:
 			'fallback': 'Match '+str(currentMatch-2)+' SNS',
 			'color':status, 'text':slackScoutNotSent,
 		}]
-	)
+	))
 print("Sent SNS notifications.")
